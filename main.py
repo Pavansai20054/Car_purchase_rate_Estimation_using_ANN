8d033f1272e14f4325a4750c7c50df6815c34eb3
Pavan <pavansai87654321@gmail.com>
2025-01-24 15:21:23 +0530

    Added image in code

--- main.py
+++ main.py
@@ -21,34 +21,40 @@ with open('label_encoder.pkl', 'rb') as file:
 
 # Get the list of countries from the LabelEncoder's classes_ attribute
 countries = le.classes_
-    
-# User inputs
-country = st.selectbox("Select your Country:", countries)  # Display countries in selectbox
-annual_salary = st.number_input("Enter your salary:", min_value=0.0, max_value=10000000.0, value=0.0, step=0.1)
-credit_card_debt = st.number_input("Enter your Credit card debt:", min_value=0.0, max_value=1000000.0, value=0.0, step=0.1)
-networth = st.number_input("Enter your net worth:", min_value=0.0, max_value=1000000.0, value=0.0, step=0.1)
 
-# Display message that model is loaded
-st.write("Model successfully loaded and compiled.")
+col1, col2 = st.columns(2)
 
-# Button to make the prediction
-if st.button("Estimate Car Purchase Rate"):
-    # Prepare the input data for the model
-    input_data = pd.DataFrame({
-        'country': [country],
-        'annual_salary': [annual_salary],
-        'credit_card_debt': [credit_card_debt],
-        'networth': [networth]
-    })
+with col1:
+    # User inputs
+    country = st.selectbox("Select your Country:", countries)  # Display countries in selectbox
+    annual_salary = st.number_input("Enter your salary:", min_value=0.0, max_value=10000000.0, value=0.0, step=0.1)
+    credit_card_debt = st.number_input("Enter your Credit card debt:", min_value=0.0, max_value=1000000.0, value=0.0, step=0.1)
+    networth = st.number_input("Enter your net worth:", min_value=0.0, max_value=1000000.0, value=0.0, step=0.1)
 
-    # Encode the country input to integer using LabelEncoder
-    input_data['country'] = le.transform([country])[0]  # Transform the country input to an integer label
+    # Display message that model is loaded
+    st.write("Model successfully loaded and compiled.")
 
-    # Use the model to predict the car purchase rate
-    prediction = model.predict(input_data)
+    # Button to make the prediction
+    if st.button("Estimate Car Purchase Rate"):
+        # Prepare the input data for the model
+        input_data = pd.DataFrame({
+            'country': [country],
+            'annual_salary': [annual_salary],
+            'credit_card_debt': [credit_card_debt],
+            'networth': [networth]
+        })
 
-    # Scale down the prediction by a factor (for example, divide by 100,000 to make it more manageable)
-    scaled_prediction = prediction[0].item() / 100000  # Adjust the scaling factor as needed
+        # Encode the country input to integer using LabelEncoder
+        input_data['country'] = le.transform([country])[0]  # Transform the country input to an integer label
 
-    # Display the formatted prediction value with commas and 4 decimal places
-    st.write(f"The estimated car purchase rate is: ${scaled_prediction:,.4f}")
+        # Use the model to predict the car purchase rate
+        prediction = model.predict(input_data)
+
+        # Scale down the prediction by a factor (for example, divide by 100,000 to make it more manageable)
+        scaled_prediction = prediction[0].item() / 100000  # Adjust the scaling factor as needed
+
+        # Display the formatted prediction value with commas and 4 decimal places
+        st.write(f"The estimated car purchase rate is: ${scaled_prediction:,.4f}")
+
+with col2:
+    st.image("image.jpg")
